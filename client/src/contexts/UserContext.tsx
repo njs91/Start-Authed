244f8c7a9d41000fca06564ba894b0f8cf48d985
Nick <nick.smith.tsa@gmail.com>
2022-06-18 16:12:05 +0100

    Finished user context and types

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -1,45 +1,45 @@
 import React, { createContext, FC, ReactNode, useState } from 'react';
 import { getCookie } from '../utils/HelperFunctions';
 
-// why are there sometimes duplicate cookies with a different 'path'? see screenshot for details
-
-export const UserContext = createContext<UserContextType>(null);
+export const UserContext = createContext<UserContextType>({
+    user: undefined,
+    setAccount: () => {},
+});
 
 export const UserProvider: FC<UserProviderProps> = ({ children }) => {
-    const [user, setUser] = useState<UserContextType>(getAccountFromCookies());
-    const setAccount = (userData: Userdata) => {
+    const [user, setUser] = useState<User | undefined>(getAccountFromCookies());
+    const setAccount = (userData: User): void => {
         setUser(userData);
         setCookies(userData);
     };
-    return <UserContext.Provider value={{ user, setAccount }}>{children} </UserContext.Provider>;
+    return <UserContext.Provider value={{ user, setAccount }}>{children}</UserContext.Provider>;
 };
 
-const getAccountFromCookies = (): Userdata | undefined => {
+const getAccountFromCookies = (): User | undefined => {
     const [id, token, email] = [getCookie('accountId'), getCookie('accountToken'), getCookie('accountEmail')];
     if (!id || !token || !email) return;
     return { id, token, email };
 };
 
-const setCookies = ({ id, token, email }: Userdata) => {
+const setCookies: setCookiesFn = ({ id, token, email }, path = '/') => {
     const expiryDate = new Date();
     expiryDate.setDate(expiryDate.getDate() + 30); // expires in 30 days
-    document.cookie = `accountId=${id}; expires=` + expiryDate;
-    document.cookie = `accountToken=${token}; expires=` + expiryDate;
-    document.cookie = `accountEmail=${email}; expires=` + expiryDate;
+    const expiryAndPath = `expires=${expiryDate}; path=${path}`;
+    document.cookie = `accountId=${id}; ${expiryAndPath}`;
+    document.cookie = `accountToken=${token}; ${expiryAndPath}`;
+    document.cookie = `accountEmail=${email}; ${expiryAndPath}`;
 };
 
-// type User = {
-//     email: string;
-//     password: string;
-// };
-// export type UserContextType = User | null;
-type User = any;
-export type UserContextType = any;
 interface UserProviderProps {
     children: ReactNode;
 }
-type Userdata = {
+export type UserContextType = {
+    user: User | undefined;
+    setAccount: (userData: User) => void;
+};
+type User = {
     id: string;
     token: string;
     email: string;
 };
+type setCookiesFn = (arg0: User, arg1?: string) => void;
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,20 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Page } from '../components/Page';
 import { homepageMeta } from './MetaTags';
 import styles from '../css/pages/homepage.module.scss';
 import { HeroArea } from '../components/default/HeroArea';
-import { UserContextType, UserContext } from '../contexts/UserContext';
 
-const Homepage: React.VFC = () => {
-    const { user } = useContext<UserContextType>(UserContext);
-
-    React.useEffect(() => console.log('user from uE: ', user), [user]); // @todo: remove
-
-    return (
-        <Page meta={homepageMeta} clsPage={styles.homepage} fullWidth={true}>
-            <HeroArea title='Title' description='Description...' buttonText='Find Out More' buttonLink='/styles' />
-        </Page>
-    );
-};
+const Homepage: React.VFC = () => (
+    <Page meta={homepageMeta} clsPage={styles.homepage} fullWidth={true}>
+        <HeroArea title='Title' description='Description...' buttonText='Find Out More' buttonLink='/styles' />
+    </Page>
+);
 
 export default Homepage;
