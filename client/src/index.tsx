7af6c12f83a55fc842eac1bfa6c086d48812ba7d
Nick <nick.smith.tsa@gmail.com>
2022-06-19 20:55:21 +0100

    Reactdom deprecation warning fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Modal from 'react-modal';
+import { createRoot } from 'react-dom/client';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = createRoot(rootElement);
 
 Modal.setAppElement('#root');
 
-ReactDOM.render(
-    <React.StrictMode>
+root.render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
