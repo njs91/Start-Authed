d3b3933355ad9cad508779b4bf200576700e7d72
Nick <nick.smith.tsa@gmail.com>
2022-07-01 20:14:31 +0100

    Reusable variables in beforeEach

--- client/src/tests/Account.test.js
+++ client/src/tests/Account.test.js
@@ -3,30 +3,31 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('create account tests', () => {
+    let inputs, labels;
+
     beforeEach(() => {
         render(
             <BrowserRouter>
                 <CreateAccountForm />
             </BrowserRouter>
         );
-    });
 
-    it('should render all inputs', () => {
-        const inputs = [
+        inputs = [
             screen.getByRole('textbox', { name: 'Email:' }),
             screen.getByLabelText('Password:'),
             screen.getByLabelText('Re-enter Password:'),
             screen.getByRole('button', { type: 'submit' }),
         ];
+        labels = document.querySelectorAll('form label');
+    });
 
+    it('should render all inputs', () => {
         inputs.forEach((input) => {
             expect(input).toBeInTheDocument();
         });
     });
 
     it('should render all labels', () => {
-        const labels = document.querySelectorAll('form label');
-
         labels.forEach((label) => {
             expect(label).toBeInTheDocument();
         });
