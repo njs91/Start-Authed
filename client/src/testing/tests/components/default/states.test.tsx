733d84c7988d1fdcb41e5bd36de12c7b964c571c
Nick <nick.smith.tsa@gmail.com>
2022-07-13 21:43:48 +0100

    Container and rerender in beforeEach without destructuring

--- client/src/testing/tests/components/default/states.test.tsx
+++ client/src/testing/tests/components/default/states.test.tsx
@@ -2,15 +2,20 @@ import { render, screen } from '@testing-library/react';
 import { Error } from '../../../../components/default/States';
 
 describe('error component', () => {
+    let container: HTMLElement, rerender: any;
+
+    beforeEach(() => {
+        const rendered = render(<Error msg={'Error found'} />);
+        [container, rerender] = [rendered.container, rendered.rerender];
+    });
+
     it('should render message', () => {
-        render(<Error msg={'Error found'} />);
         const message = screen.getByText(/error found/i);
 
         expect(message).toBeInTheDocument();
     });
 
     it('should render class names', () => {
-        const { container, rerender } = render(<Error msg={'Error found'} />);
         const initialClasses = 'situationText error';
         const wrappingElement = container.firstChild;
 
