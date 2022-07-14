8253ca4edb161ebcb34628fa943610f19db3b151
Nick <nick.smith.tsa@gmail.com>
2022-07-14 07:54:26 +0100

    Success component tests

--- client/src/testing/tests/components/default/states.test.tsx
+++ client/src/testing/tests/components/default/states.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Error } from '../../../../components/default/States';
+import { Error, Success } from '../../../../components/default/States';
 
 describe('error component', () => {
     let container: HTMLElement, rerender: any;
@@ -26,6 +26,36 @@ describe('error component', () => {
 
     it('should render icon', () => {
         const icon = container.querySelector("svg[data-icon='triangle-exclamation']");
+
+        expect(icon).toBeInTheDocument();
+    });
+});
+
+describe('success component', () => {
+    let container: HTMLElement, rerender: any;
+
+    beforeEach(() => {
+        const rendered = render(<Success msg={'Success message'} />);
+        [container, rerender] = [rendered.container, rendered.rerender];
+    });
+
+    it('should render message', () => {
+        const message = screen.getByText(/success message/i);
+
+        expect(message).toBeInTheDocument();
+    });
+
+    it('should render class names', () => {
+        const initialClasses = 'situationText success';
+        const wrappingElement = container.firstChild;
+
+        expect(wrappingElement).toHaveClass(initialClasses);
+        rerender(<Success msg={'Success message'} cls='testcls' marginTop={true} />);
+        expect(wrappingElement).toHaveClass(`${initialClasses} testcls marginTop`);
+    });
+
+    it('should render icon', () => {
+        const icon = container.querySelector("svg[data-icon='circle-check']");
 
         expect(icon).toBeInTheDocument();
     });
