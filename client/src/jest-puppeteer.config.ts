0cfb6903c0806baac420aa2544e711b0e4052e63
Nick <nick.smith.tsa@gmail.com>
2022-07-22 20:28:45 +0100

    Jest-puppeteer setup with login e2e tests

---
+++ client/src/jest-puppeteer.config.ts
@@ -0,0 +1,3 @@
+export const launch = {
+    headless: false,
+};
---
+++ client/src/testing/fixtures/user.ts
@@ -0,0 +1,11 @@
+export const wrongUser = {
+    email: 'wrong@user.com',
+    password: 'wrong',
+    id: '12345',
+};
+
+export const realUser = {
+    email: 'a@a.a',
+    password: 'a',
+    id: '62d1d8ec7ce7d45bfe3e9fc3',
+};
