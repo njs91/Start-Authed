224e800abb378893d60a9afd60b2a52b0c5ce490
Nick <nick.smith.tsa@gmail.com>
2022-07-26 23:36:21 +0100

    Forgot types

--- client/src/scripts/affiliates.ts
+++ client/src/scripts/affiliates.ts
@@ -1,12 +1,12 @@
 import { getCookie } from '../utils/HelperFunctions';
 
-export const setAffiliateCookies = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const referrer = urlParams.get('referrer');
-    const cookieExists = Boolean(getCookie('referrer')); // to only credit initial referrer
+export const setAffiliateCookies: () => void = () => {
+    const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+    const referrer: string | null = urlParams.get('referrer');
+    const cookieExists: boolean = Boolean(getCookie('referrer')); // to only credit initial referrer
 
     if (!referrer || cookieExists) return;
 
-    const expiryDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1)); // 1 year from now
+    const expiryDate: Date = new Date(new Date().setFullYear(new Date().getFullYear() + 1)); // 1 year from now
     document.cookie = `referrer=${referrer}; expires=${expiryDate.toUTCString()};`;
 };
